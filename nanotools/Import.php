b25f238b912b1033fc946f8d9543051950280f2e
Jan Toth <yanchi.toth@gmail.com>
2014-02-09 10:28:45 +0100

    Docs and tiny refactoring

--- nanotools/Import.php
+++ nanotools/Import.php
@@ -6,19 +6,24 @@ class Import {
 
     protected $directory;
 
-    public static final function directory($directory_name) {
-        if (empty($directory_name)) {
-            throw new Exception("Undefined directory name");
+    /**
+     * Registers an import for the provided directory. Does not explore recursively.
+     * @param string $directory The directory to search.
+     * @throws Exception
+     */
+    public static final function directory($directory) {
+        if (empty($directory) || !file_exists($directory) || !is_dir($directory)) {
+            throw new Exception("Directory does not exist: $directory.");
         }
-        spl_autoload_register([new Import($directory_name), 'load']);
+        spl_autoload_register([new Import($directory), 'load']);
     }
 
-    private final function __construct($directory_name) {
-        $this->directory = $directory_name;
+    private final function __construct($directory) {
+        $this->directory = $directory;
     }
 
-    public function load($classname) {
-        $filename = $classname . '.php';
+    public function load($class) {
+        $filename = $class . '.php';
         $path = $this->directory . self::DIRECTORY_SEPARATOR . $filename;
         if (file_exists($path)) { // Allow other loaders to include the file.
             require($path);
--- nanotools/Template.php
+++ nanotools/Template.php
@@ -9,27 +9,42 @@ class Template {
 
     private $vars = [];
 
+    /**
+     * Create the template with pre-set view directory and a layout file in that directory.
+     * @param string $viewDirectory The directory in which to find views.
+     * @param string $layoutFileName The layout script, without php extension.
+     * @throws Exception When the provided paths are incorrect.
+     */
     public final function __construct($viewDirectory, $layoutFileName) {
         if (empty($viewDirectory) || !file_exists($viewDirectory) || !is_dir($viewDirectory)) {
             throw new Exception("Directory does not exist: $viewDirectory.");
         }
+        $this->viewDirectory = $viewDirectory;
 
         if (empty($layoutFileName)) {
             throw new Exception("No layout file provided.");
         }
+        $layoutFile = $this->getScriptFile($layoutFileName);
+        $this->checkFileExists($layoutFile);
 
-        $this->viewDirectory = $viewDirectory;
-        $this->layoutFile = $this->viewDirectory . self::DIRECTORY_SEPARATOR . $layoutFileName . '.php';
-
-        $this->checkFileExists($this->layoutFile);
+        $this->layoutFile = $layoutFile;
     }
 
+    /**
+     * Assign a variable to be visible in view script.
+     * @param string $key The variable name.
+     * @param object $value The variable value.
+     */
     public function assign($key, $value) {
         $this->vars[$key] = $value;
     }
 
+    /**
+     * Render a view script without layout.
+     * @param string $script The script name, without php extension.
+     */
     public function renderPartial($script) {
-        $scriptFile = $this->viewDirectory . self::DIRECTORY_SEPARATOR . $script . '.php';
+        $scriptFile = $this->getScriptFile($script);
         $this->checkFileExists($scriptFile);
 
         // Set assigned vars for local script scope.
@@ -40,6 +55,10 @@ class Template {
         require $scriptFile;
     }
 
+    /**
+     * Render a script with layout. The layout must implicitly render the script via echo $content.
+     * @param string $script The script name, without php extension.
+     */
     public function render($script) {
         $scriptFile = $this->getScriptFile($script);
         $this->checkFileExists($scriptFile);
