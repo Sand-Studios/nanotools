9e8d14c5445aa9d14a20b975bbfe3be0e23810c0
Yanchi Toth <yanchi.toth@gmail.com>
2014-02-19 14:08:04 +0100

    Added psr0 autoloading

--- nanotools/Import.php
+++ nanotools/Import.php
@@ -2,31 +2,70 @@
 
 class Import {
 
-    const DIRECTORY_SEPARATOR = '/';
+    const NAMESPACE_SEPARATOR = '\\';
 
-    protected $directory;
-
-    /**
-     * Registers an import for the provided directory. Does not explore recursively.
-     * @param string $directory The directory to search.
-     * @throws Exception
-     */
-    public static final function directory($directory) {
-        if (empty($directory) || !file_exists($directory) || !is_dir($directory)) {
-            throw new Exception("Directory does not exist: $directory.");
-        }
-        spl_autoload_register([new Import($directory), 'load']);
-    }
+    private $directory;
 
     private final function __construct($directory) {
         $this->directory = $directory;
     }
 
-    public function load($class) {
-        $filename = $class . '.php';
-        $path = $this->directory . self::DIRECTORY_SEPARATOR . $filename;
-        if (file_exists($path)) { // Allow other loaders to include the file.
+    /**
+     * Registers a custom autoload handler.
+     * @param callable $handler The custom handler.
+     */
+    public static final function handler(callable $handler) {
+        spl_autoload_register($handler);
+    }
+
+    /**
+     * Registers an autoloader for the provided directory. Does not explore recursively.
+     * @param string $directory The directory to search.
+     * @throws Exception When directory does not exist.
+     */
+    public static final function directory($directory) {
+        self::checkDirecory($directory);
+        spl_autoload_register([new self($directory), 'loadClassFromDirectory']);
+    }
+
+    /**
+     * Registers an autoloader for the provided directory. Loads based on namespaces and PSR0.
+     * @param string $directory The directory to search.
+     * @throws Exception When directory does not exist.
+     */
+    public static final function namespaced($directory) {
+        self::checkDirecory($directory);
+        spl_autoload_register([new self($directory), 'loadClassFromNamespaceRoot']);
+    }
+
+    private static function checkDirecory($directory) {
+        if (empty($directory) || !file_exists($directory) || !is_dir($directory)) {
+            throw new Exception("Directory does not exist: $directory.");
+        }
+    }
+
+    public function loadClassFromDirectory($className) {
+        $filename = $className . '.php';
+        $path = $this->directory . DIRECTORY_SEPARATOR . $filename;
+        if (is_readable($path)) { // Allow other loaders to include the file.
             require($path);
         }
     }
+
+    public function loadClassFromNamespaceRoot($className) {
+        $namespacePath = '';
+        $lastNamespacePosition = strripos($className, self::NAMESPACE_SEPARATOR);
+        if (false !== $lastNamespacePosition) {
+            $namespace = substr($className, 0, $lastNamespacePosition);
+            $className = substr($className, $lastNamespacePosition + 1);
+            $namespacePath = str_replace(self::NAMESPACE_SEPARATOR, DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+        }
+        $namespacePath .= str_replace('_', DIRECTORY_SEPARATOR, $className) . 'php';
+
+        $path = (is_null($this->directory) ? '' : $this->directory . DIRECTORY_SEPARATOR) . $namespacePath;
+        if (is_readable($path)) { // Allow other loaders to include the file.
+            require $path;
+        }
+    }
+
 }
